0567534baf3e4e2af11406e9148d63c6f3bb8249
libinj <libinj@yonyou.com>
2018-08-10 15:44:35 +0800

    提交内容：养老调剂金方法查询修改显示返回 提交人：李彬 提交时间：2018010 1233

--- src/main/java/com/ufgov/sssfm/project/module/pensionAdjust/controller/PensionAdjustController.java
+++ src/main/java/com/ufgov/sssfm/project/module/pensionAdjust/controller/PensionAdjustController.java
@@ -54,26 +54,29 @@ public class PensionAdjustController {
                         FmAdjustGold fmAdjustGold = JSON.parseObject(pensionAdjustJson,FmAdjustGold.class);
                         FmAdjustGoldItem fmAdjustGoldItem = JSON.parseObject(pensionAdjustJson,FmAdjustGoldItem.class);
                         //判断是否存在拨款单Id,如果存在，则先删除，后插入。不存在，直接插入
-                        if((fmAdjustGoldItem.getId().length()<=0)){
-                                String id = UUID.randomUUID()+"";
+                        String id = UUID.randomUUID()+"";
+                        if((fmAdjustGold.getId().length()<=0)){
                                 fmAdjustGold.setId(id);
-                                fmAdjustGoldItem.setId(id);
-                                pensionAdjustService.insert_fmAdjustGoldItem(fmAdjustGoldItem);
                                 pensionAdjustService.insert_fmAdjustGold(fmAdjustGold);
                         }else{
                                 //删除
-                                pensionAdjustService.deletefmAdjustGoldItemByPK(fmAdjustGoldItem.getId());
                                 pensionAdjustService.deletefmAdjustGoldByPK(fmAdjustGold.getId());
                                 //插入
-                                pensionAdjustService.insert_fmAdjustGoldItem(fmAdjustGoldItem);
                                 pensionAdjustService.insert_fmAdjustGold(fmAdjustGold);
                         }
-
+                        if((fmAdjustGoldItem.getId().length()<=0)){
+                            fmAdjustGoldItem.setId(id);
+                            pensionAdjustService.insert_fmAdjustGoldItem(fmAdjustGoldItem);
+                        }else{
+                            //删除
+                            pensionAdjustService.deletefmAdjustGoldItemByPK(fmAdjustGoldItem.getId());
+                            //插入
+                            pensionAdjustService.insert_fmAdjustGoldItem(fmAdjustGoldItem);
+                        }
                 }catch (Exception e){
                         jsonObject.put("result","插入数据库失败");
                         return jsonObject.toString();
                 }
-
                 jsonObject.put("result","生成养老调剂金单成功");
                 return jsonObject.toString();
 
