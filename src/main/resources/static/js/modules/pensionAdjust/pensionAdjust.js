4b889430f0c6e04bb21cd4a9cdbdf3030ea68534
hwq <1224943007@qq.com>
2018-08-11 10:33:58 +0800

    养老保险调剂金调整

--- src/main/resources/static/js/modules/pensionAdjust/pensionAdjust.js
+++ src/main/resources/static/js/modules/pensionAdjust/pensionAdjust.js
@@ -8,8 +8,8 @@ $(function(){
         del:'../../../persionAdjust/PersionAdjustController/delete_persionAdjust',//删除
         sh:'../../../persionAdjust/PersionAdjustController/shenhe_persionAdjust'//审核
     };
-    var cols=[    //表头
-        // {field: 'ck', checkbox: true},//checkbox列
+    var colOne=[    //表头
+        {field: 'ck', checkbox: true},//checkbox列
         {
             field: 'number', title: '序号', align: 'center', formatter: function (value, row, index) {
                 return index + 1;
@@ -27,16 +27,37 @@ $(function(){
         {field: 'SBKZS', title: '社保局审核预计动用历年累计结余控制数', align: 'right',halign:'center',editable:{type:'text'}},
         {field: 'SXJE', title: '市县申请调剂金额',align: 'right',halign:'center',editable:{type:'text'}},
         {field: 'SBJE', title: '社保局审核调剂金额', align: 'right',halign:'center',editable:{type:'text'}},
-        {field: 'CZJE', title: '财政厅核定调剂金额', align: 'right',halign:'center',editable:{type:'text'}},
-        {field: 'DELID', title: '删除行', align: 'center',visible:false,formatter(value,row,index){return value+1}}//删除一行预定ID列
+        {field: 'CZJE', title: '财政厅核定调剂金额', align: 'right',halign:'center',editable:{type:'text'}}
+    ];
+
+    var colTwo=[    //表头
+        {
+            field: 'number', title: '序号', align: 'center', formatter: function (value, row, index) {
+                return index + 1;
+            }
+        },
+        {field: 'DWMC', title: '单位名称', align: 'center'},
+        {field: 'SNJJJY', title: '上年末基金累计结余', align: 'right',halign:'center'},
+        {field: 'DNJJSR', title: '当年基金预算收入', align: 'right',halign:'center'},
+        {field: 'DNJJZC', title: '当年基金预算支出', align: 'right',halign:'center'},
+        {field: 'YSJJJE', title: '预算应调剂补助基金金额', align: 'right',halign:'center'},
+        {field: 'YJJJJY', title: '预计当年基金结余', align: 'right',halign:'center'},
+        {field: 'YJJJSR', title: '预计当年基金征缴收入', align: 'right',halign:'center'},
+        {field: 'DNYJZC', title: '当年基金预计支出', align: 'right',halign:'center'},
+        {field: 'YJSZJY', title: '预计当年收支结余', align: 'right',halign:'center'},
+        {field: 'SBKZS', title: '社保局审核预计动用历年累计结余控制数', align: 'right',halign:'center'},
+        {field: 'SXJE', title: '市县申请调剂金额',align: 'right',halign:'center'},
+        {field: 'SBJE', title: '社保局审核调剂金额', align: 'right',halign:'center'},
+        {field: 'CZJE', title: '财政厅核定调剂金额', align: 'right',halign:'center'}
     ];
 
     //单元格按钮事件
     window.operateEvents = {
         'click .btn-edit':function (e, value, row, index) {//修改
-
             $('#firstTable').bootstrapTable('uncheckAll');
             $('#win').modal('show');
+            $('#btn-save,.appendRow').removeClass('hide');
+            $('#win input').attr('readonly',false);
             $('#myModalLabel').html('编辑审核表');
             $.ajax({
                 url: allUrl.edit,
@@ -62,10 +83,11 @@ $(function(){
                         pageSize: 10,
                         pagination: true, // 是否分页
                         clickToSelect:true,
+                        singleSelect:true,
                         sidePagination: 'server',//server:服务器端分页|client：前端分页
                         paginationHAlign: 'left',//分页条水平方向的位置，默认right（最右），可选left
                         paginationDetailHAlign: 'right',//paginationDetail就是“显示第 1 到第 8 条记录，总共 15 条记录 每页显示 8 条记录”，默认left（最左），可选right
-                        columns:cols
+                        columns:colOne
                     });
                     $('#year').val(result.result[0].YEAR);
                     $('#quarter').val(result.result[0].QUARTER);
@@ -86,7 +108,9 @@ $(function(){
                     dataType:'json',
                     data:{
                         id:row.ID,
-                        spStatus:'01'
+                        spStatus:'01',
+                        sp_name:'黄制单',
+                        sp_status_name:'制单'
                     },
                     beforeSend:function (){
                         $('#myModal').modal('show');
@@ -123,6 +147,47 @@ $(function(){
                     }
                 });
             }
+        },
+        'click .btn-see':function (e, value, row, index){//查看
+            $('#firstTable').bootstrapTable('uncheckAll');
+            $('#win').modal('show');
+            $('#win input').attr('readonly','readonly');//input不可编辑
+            $('#btn-save,.appendRow').addClass('hide');
+            $('#myModalLabel').html('查看审核表');
+            $.ajax({
+                url: allUrl.edit,
+                type:"post",
+                dataType:'json',
+                data:{
+                    id:row.ID
+                },
+                beforeSend:function (){
+                    $('#myModal').modal('show');
+                },
+                success: function(result){
+                    $('#myModal').modal('hide');
+                    $('#modalTable').bootstrapTable('destroy').bootstrapTable({
+                        url: allUrl.edit,
+                        queryParams: {
+                            id:row.ID
+                        },
+                        method: 'post',
+                        contentType: "application/x-www-form-urlencoded",//当请求方法为post的时候,必须要有！！！！
+                        dataField: "TABLE",//定义从后台接收的字段，包括result和total，这里我们取result
+                        pageNumber: 1, //初始化加载第一页
+                        pageSize: 10,
+                        pagination: true, // 是否分页
+                        sidePagination: 'server',//server:服务器端分页|client：前端分页
+                        paginationHAlign: 'left',//分页条水平方向的位置，默认right（最右），可选left
+                        paginationDetailHAlign: 'right',//paginationDetail就是“显示第 1 到第 8 条记录，总共 15 条记录 每页显示 8 条记录”，默认left（最左），可选right
+                        columns:colTwo
+                    });
+                    $('#year').val(result.result[0].YEAR);
+                    $('#quarter').val(result.result[0].QUARTER);
+                    $('#bzDate').val(result.result[0].BZDATE);
+                    $('#shDate').val(result.result[0].SHDATE);
+                }
+            });
         },
         'click .btn-sh':function (e, value, row, index) {//审核
             $('#firstTable').bootstrapTable('uncheckAll');
@@ -193,6 +258,7 @@ $(function(){
                                 return '<button class="btn btn-primary btn-edit">修改</button>&nbsp;'+
                                 '<button class="btn btn-primary btn-submit">提交</button>&nbsp;'+
                                 '<button class="btn btn-primary btn-del">删除</button>&nbsp;'+
+                                '<button class="btn btn-primary btn-see">查看</button>&nbsp;'+
                                 '<button class="btn btn-primary btn-sh">审核</button>&nbsp;'+
                                 '<button class="btn btn-primary btn-disagree">驳回</button>'
                             },
@@ -212,7 +278,7 @@ $(function(){
                     sidePagination: 'server',//server:服务器端分页|client：前端分页
                     paginationHAlign: 'left',//分页条水平方向的位置，默认right（最右），可选left
                     paginationDetailHAlign: 'right',//paginationDetail就是“显示第 1 到第 8 条记录，总共 15 条记录 每页显示 8 条记录”，默认left（最左），可选right
-                    columns:cols,
+                    columns:colOne,
                     data:[]
                 });
             },
@@ -242,8 +308,9 @@ $(function(){
                 $('#btn-add').on('click',function () {
                     $('#firstTable').bootstrapTable('uncheckAll');
                     $('#win').modal('show');
+                    $('#btn-save,.appendRow').removeClass('hide');
+                    $('#win input').val('').attr('readonly',false);
                     $('#myModalLabel').html('新增审核表');
-                    $('#win input').val('');//新增置空
                     $('#win td span').html('');//新增置空
                     page.getWinTab();//重绘Bootstrap
                 });
@@ -264,14 +331,21 @@ $(function(){
                             'SBKZS':'',
                             'SXJE':'',
                             'SBJE':'',
-                            'CZJE':'',
-                            'DELID':$('#modalTable').bootstrapTable('getData').length+1
+                            'CZJE':''
                     }})
                 });
 
                 //删除可编辑表格最后一行
                 $('.btn-minus').on('click',function () {
-                    $('#modalTable').bootstrapTable('remove',{field:'DELID',values:[$('#modalTable').bootstrapTable('getData').length]})
+                    if($('#firstTable').bootstrapTable('getSelections')[0].SP_STATUS!='00'){
+                        commonJS.confirm('警告','已经提交不可删除！');
+                    }
+                    else if($('#modalTable').bootstrapTable('getSelections').length==0){
+                        commonJS.confirm('警告','请选择一条数据删除！');
+                    }
+                    else{
+                        $('#modalTable').bootstrapTable('remove',{field:'DELID',values:[$('#modalTable').bootstrapTable('getData').length]})
+                    }
                 });
 
                 //保存
