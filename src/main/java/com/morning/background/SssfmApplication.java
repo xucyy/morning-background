0d1cc815e78f4f4a9e4004c06266042834f7c619
xucy <xucy@yonyou.com>
2019-04-11 14:06:01 +0800

    测试git

--- src/main/java/com/morning/background/SssfmApplication.java
+++ src/main/java/com/morning/background/SssfmApplication.java
@@ -1,17 +1,15 @@
-package com.ufgov.sssfm;
+package com.morning.background;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * 启动程序
  *
- * @author ruoyi
+ * @author xucy
  */
 @SpringBootApplication
 @EnableTransactionManagement
